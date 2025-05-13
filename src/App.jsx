06d39df6bd99e9a7d21af45ef5a542f5bef43545
Gil Steiner <games.gil@gmail.com>
2025-05-13 06:44:54 +0300

    - split into component - add states

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,44 @@
 import { useState } from "react";
+import Button from './ui/Button';
+import NoProjectSelected from "./ui/NoProjectSelected";
 
 function App() {
   const [projects, setProjects] = useState([]);
+  const [selectedProject, setSelectedProject] = useState(-1);
+  const [mode, setMode] = useState();
+
+  function addProject() {
+    setProjects([...projects, {
+      id: Math.random(),
+      title: 'A Project'
+    }])
+  }
 
   return (
     <>
       <aside>
-        <h2 class="uppercase">your projects</h2>
-        <button>Add Project</button>
+        <h2 className="uppercase">your projects</h2>
+        <Button text='+ Add Project' onClick={addProject}></Button>
         {projects.length ? (
           <ul>
             {projects.map((project) => (
-              <li>one</li>
+              <li key={project.id}>{project.title}</li>
             ))}
           </ul>
         ) : null}
       </aside>
 
-      <section>
-        {projects.length ? (
+      <section className="flex flex-col items-center justify-center">
+        {selectedProject !== -1 ? (
           <>
-            <div>
+            <div className="flex flex-col items-center justify-center">
               <h2>Learning React</h2>
-              <button>Delete</button>
+              <Button text='Delete'></Button>
             </div>
             <span>Some text...</span>
           </>
         ) : (
-          <div class="flex flex-col items-center justify-center">
-            <img src="src/assets/no-projects.png" alt="" width="100" />
-            <h3 class="font-bold text-2xl">No Project Selected</h3>
-            <span class="text-xl">Select a project or get started with a new one</span>
-            <button class="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">Create new project</button>
-
-          </div>
+          <NoProjectSelected />
         )}
       </section>
     </>
---
+++ src/ui/NoProjectSelected.jsx
@@ -0,0 +1,10 @@
+import Button from './Button';
+
+export default function NoProjectSelected() {
+    return (<div className="flex flex-col items-center justify-center">
+            <img src="src/assets/no-projects.png" alt="" width="100" />
+            <h3 className="font-bold text-2xl">No Project Selected</h3>
+            <span className="text-xl">Select a project or get started with a new one</span>
+            <Button text='Create new project'></Button>
+          </div>)
+}
